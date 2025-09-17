e3a76b79ece0941d86d532a1fd03f6a8dd65963a
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-09-17 18:02:04 -0400

    Temporarily fix datepicker

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,5 @@
 import moment from 'moment';
 import Chart from 'chart.js/auto';
-import datepicker from 'bootstrap-datepicker';
-
 
 window.moment = moment;
 window.Chart = Chart;
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import $ from 'jquery';
 import 'jquery-ui';
+import 'bootstrap-datepicker';
+
 
 window.$ = window.jQuery = $;
 
