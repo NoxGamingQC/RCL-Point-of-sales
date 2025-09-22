11705d57653f3456b4b4d0c9744a690360b9c950
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-09-22 12:35:37 -0400

    Update with prompt

--- update_with_prompt.bat
+++ update_with_prompt.bat
@@ -4,9 +4,12 @@ echo Mise a jour des fichiers du site...
 git pull origin master
 echo Mise a jour des fichiers du site complete.
 
+start cmd /k "yarn install"
+
+
 echo Generation du design du site à partir des fichiers mis à jour...
 @echo off
-yarn run mix
+start cmd /k "yarn run dev"
 echo Generation complete.
 @echo on
 
