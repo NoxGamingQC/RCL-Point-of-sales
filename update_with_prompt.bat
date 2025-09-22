6f8c847c5059e0891e149a06ba116f234708d447
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-09-22 12:09:44 -0400

    Update with prompt

--- update_with_prompt.bat
+++ update_with_prompt.bat
@@ -1,10 +1,39 @@
 @echo on
 
-cd %WorkingDirectory%
-git pull origin master
-yarn install
-npm run dev
+SETLOCAL EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
+  set "DEL=%%a"
+)
 
-echo Mise à jour du site et de la caisse complété. Si des erreurs apparaisse plus haut, merci de contacter Service Tech. J.Bédard
+call :colorEcho 0a "Mise à jour des fichiers du site..."
+echo.
+echo:
+git pull origin master
+echo:
+call :colorEcho 0a "Mise à jour des fichiers du site complété."
+echo.
+echo:
+
+call :colorEcho 0a "Mise à jour des dépendance NPM"
+echo.
+echo:
+yarn install
+call :colorEcho 0a "Mise à jour des dépendance NPM complété."
+echo.
+echo:
+
+call :colorEcho 0a "Génération du désign du site à partir des fichiers mis à jour..."
+echo.
+npm run dev
+call :colorEcho 0a "Génération complété."
+echo.
+
+echo:
+call :colorEcho 0e "Mise à jour complété. (Si des erreurs sont survenus, merci d'en aviser Service Tech. J.Bédard)"
+echo.
+echo:
+
+pause
 
 :Exit
+exit
