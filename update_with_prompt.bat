c07dd8abb8bebd8620d0052b182fef75fdf23397
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-09-22 12:19:25 -0400

    Update with prompt

--- update_with_prompt.bat
+++ update_with_prompt.bat
@@ -3,28 +3,28 @@
 color 0A
 echo Mise à jour des fichiers du site...
 color 0F
-echo :
+echo:
 git pull origin master
-echo :
+echo:
 color 0A
 echo Mise à jour des fichiers du site complété.
 
 echo Mise à jour des dépendance NPM
 color 0F
-echo :
+echo:
 yarn install
-echo :
+echo:
 color 0A
 echo Mise à jour des dépendance NPM complété.
 
 echo Génération du désign du site à partir des fichiers mis à jour...
 color 0F
-echo :
+echo:
 npm run dev
-echo :
+echo:
 color 0A
 echo Génération complété.
-echo :
+echo:
 
 color 0E
 echo Mise à jour complété. (Si des erreurs sont survenus, merci d'en aviser Service Tech. J.Bédard)
