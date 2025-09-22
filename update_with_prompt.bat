435067b93f8502cc80fdc46d9efbce4bc50a5f3d
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-09-22 12:27:20 -0400

    Update with prompt

--- update_with_prompt.bat
+++ update_with_prompt.bat
@@ -3,11 +3,12 @@
 echo Mise à jour des fichiers du site...
 git pull origin master
 echo Mise à jour des fichiers du site complété.
+
 pause
 
-echo Mise à jour des dépendance NPM
-yarn install
-echo Mise à jour des dépendance NPM complété.
+REM echo Mise à jour des dépendance NPM
+REM yarn install
+REM echo Mise à jour des dépendance NPM complété.
 
 echo Génération du désign du site à partir des fichiers mis à jour...
 npm run dev
