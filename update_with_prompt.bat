31d8390a5729257911b446960df6f73f82bafcda
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-09-22 12:42:42 -0400

    Fix update prompt

--- update_with_prompt.bat
+++ update_with_prompt.bat
@@ -7,13 +7,15 @@ echo Mise a jour des fichiers du site complete.
 
 
 echo Installations des dependances...
+start /wait cmd /c "composer install"
 start /wait cmd /c "yarn install"
 echo Dependances intalles
 
 
-echo Generation du design du site à partir des fichiers mis à jour...
+echo Generation du design du site a partir des fichiers mis a jour...
 start /wait cmd /c "yarn run dev"
 echo Generation complete.
 
-echo Une fois tout les autres terminals fermés, la mise à jour sera complété.
+echo
+echo Mise a jour complete.
 pause
