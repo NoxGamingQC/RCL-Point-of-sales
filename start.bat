064b227674907c0b1bd96d0d0e4fa42413a88c4b
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-09-25 18:32:40 -0400

    Remove pop up temporarly

--- start.bat
+++ start.bat
@@ -26,7 +26,7 @@ if %ERRORLEVEL%==0 (
 :SilentCall
 cd %WorkingDirectory%
 git pull origin master
-start cmd /c "php artisan schedule:work"
+REM start cmd /c "php artisan schedule:work"
 php artisan serve --host 192.168.2.13 --port 80
 
 
