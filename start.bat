19f0bb57022f21a2b269b7077757d302668903fe
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-09-25 21:35:50 -0400

    Fix port

--- start.bat
+++ start.bat
@@ -1,33 +1,32 @@
 @echo off
 
-REM if [%1]==[] (
-REM     goto PreSilentCall
-REM ) else (
-REM     goto SilentCall
-REM )
+if [%1]==[] (
+    goto PreSilentCall
+) else (
+    goto SilentCall
+)
 
-REM :PreSilentCall
+:PreSilentCall
 REM Insert code here you want to have happen BEFORE this same .bat file is called silently
 REM such as setting paths like the below two lines
 
-REM set WorkingDirWithSlash=%~dp0
-REM set WorkingDirectory=%WorkingDirWithSlash:~0,-1%
+set WorkingDirWithSlash=%~dp0
+set WorkingDirectory=%WorkingDirWithSlash:~0,-1%
 
 REM below code will run this same file silently, but will go to the SilentCall section
-REM cd C:\Windows\System32
-REM if exist C:\Windows\Temp\invis.vbs ( del C:\Windows\Temp\invis.vbs /f /q )
-REM echo CreateObject("Wscript.Shell").Run "C:\POS\start.bat " ^& WScript.Arguments(0), 0, False > C:\Windows\Temp\invis.vbs
-REM wscript.exe C:\Windows\Temp\invis.vbs Initialized
-REM if %ERRORLEVEL%==0 (
-REM     echo Successfully started SilentCall code. This command prompt can now be exited.
-REM     goto Exit
-REM )
+cd C:\Windows\System32
+if exist C:\Windows\Temp\invis.vbs ( del C:\Windows\Temp\invis.vbs /f /q )
+echo CreateObject("Wscript.Shell").Run "C:\POS\start.bat " ^& WScript.Arguments(0), 0, False > C:\Windows\Temp\invis.vbs
+wscript.exe C:\Windows\Temp\invis.vbs Initialized
+if %ERRORLEVEL%==0 (
+    echo Successfully started SilentCall code. This command prompt can now be exited.
+    goto Exit
+)
 
 
-REM :SilentCall
+:SilentCall
 cd %WorkingDirectory%
 REM start cmd /c "php artisan schedule:work"
-REM net stop http /y
 git pull origin master
 :retry
 for /f "tokens=5" %%p in ('netstat -ano ^| findstr ":80"') do (
