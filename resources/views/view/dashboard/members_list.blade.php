1c25cc2dbe50bdd6b36d6aa098a40f7bafb1aa17
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-10-16 19:51:41 -0400

    Fix member list printing

--- resources/views/view/dashboard/members_list.blade.php
+++ resources/views/view/dashboard/members_list.blade.php
@@ -15,7 +15,7 @@
 </style>
 <div class="container-fluid">
     <br />
-    <h1>Liste des membres</h1> 
+    <h1>Liste des membres en règle</h1> 
     <br />
     <table class="table table-bordered">
         <thead>
@@ -24,18 +24,20 @@
                 <th>Prénom</th>
                 <th>Téléphone</th>
                 <th>Courriel</th>
-                <th>Année payé</th>
+                <th class="no-print">Année payé</th>
             </tr>
         </thead>
         <tbody>
             @foreach($members as $member)
-            <tr class="{{ $member->last_year_paid < date('Y') ? 'table-danger' : ($member->last_year_paid == date('Y') ? 'table-warning' : ($member->last_year_paid > date('Y') ? 'table-success' : '')) }}">
-                <td>{{ $member->lastname }}</td>
-                <td>{{ $member->firstname }}</td>
-                <td>{{ $member->phone_number }}</td>
-                <td>{{ $member->email_address }}</td>
-                <td>{{ $member->last_year_paid }}</td>
-            </tr>
+                @if(!$member->archive)
+                    <tr class="{{ $member->last_year_paid < date('Y') ? 'table-danger' : ($member->last_year_paid == date('Y') ? 'table-warning' : ($member->last_year_paid > date('Y') ? 'table-success' : '')) }}">
+                        <td>{{ $member->lastname }}</td>
+                        <td>{{ $member->firstname }}</td>
+                        <td>{{ $member->phone_number }}</td>
+                        <td>{{ $member->email_address }}</td>
+                        <td class="no-print">{{ $member->last_year_paid }}</td>
+                    </tr>
+                @endif
             @endforeach
         </tbody>
     </table>
