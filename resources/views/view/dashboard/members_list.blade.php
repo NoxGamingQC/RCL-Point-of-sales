b6c9ea1c343ac5af416434fb4cbbd370ff7c7cf7
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-10-16 20:23:10 -0400

    Add info

--- resources/views/view/dashboard/members_list.blade.php
+++ resources/views/view/dashboard/members_list.blade.php
@@ -15,27 +15,29 @@
 </style>
 <div class="container-fluid">
     <br />
-    <h1>Liste des membres en règle</h1> 
+    <h1>Liste des membres en règle ({{date('Y')}})</h1> 
     <br />
     <table class="table table-bordered">
         <thead>
             <tr>
+                <th>No de membre</th>
                 <th>Nom</th>
                 <th>Prénom</th>
                 <th>Téléphone</th>
                 <th>Courriel</th>
-                <th class="no-print">Année payé</th>
+                <th>Année payé</th>
             </tr>
         </thead>
         <tbody>
             @foreach($members as $member)
                 @if(!$member->archive)
                     <tr class="{{ $member->last_year_paid < date('Y') ? 'table-danger' : ($member->last_year_paid == date('Y') ? 'table-warning' : ($member->last_year_paid > date('Y') ? 'table-success' : '')) }}">
+                        <td>{{ $member->member_id }}</td>
                         <td>{{ $member->lastname }}</td>
                         <td>{{ $member->firstname }}</td>
                         <td>{{ $member->phone_number }}</td>
                         <td>{{ $member->email_address }}</td>
-                        <td class="no-print">{{ $member->last_year_paid }}</td>
+                        <td>{{ $member->last_year_paid }}</td>
                     </tr>
                 @endif
             @endforeach
