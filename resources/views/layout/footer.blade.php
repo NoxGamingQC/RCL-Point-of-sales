2ad44479509dc158be28dd0f2ed21f1e02089cd9
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-10-20 13:12:51 -0400

    Make footer smaller

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,8 +1,5 @@
-<footer class="mt-auto no-print developper-font" style="background-color:#880000;color:#FFF;height:15%;padding:5%">
+<footer class="mt-auto no-print developper-font" style="background-color:#880000;color:#FFF;height:5%;padding:2%">
     <div class="col-md-12 text-center">
-        <img src="/images/logo_dev.svg" width="75px">
-        <h1 class="text-center">Crée et maintenu par Service Technologique J.Bédard</h1>
-        <h2 class="text-center">819-852-8705</h2>
-        <h3 class="text-center">&copy Service Technologique J.Bédard</h3>
+        <h1 class="text-center"><a href="https://www.jimmybedard.ca" style="color:#F5F5F5;text-decoration: none;">&copy Service Technologique J.Bédard</a> <span class="h2">- Support: 819-852-8705</span></h1>
     </div>
 </footer>
