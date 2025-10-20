2e66bdba0ae4c03edcb2392f7bc7bffbc20d77c1
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-10-20 08:19:27 -0400

    Fix pos

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -23,9 +23,11 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.10.0/js/bootstrap-datepicker.min.js" integrity="sha512-LsnSViqQyaXpD4mBBdRYeP6sRwJiJveh2ZIbW41EBrNmKxgr/LFZIiWT6yr+nycvhvauz8c2nYMhrP80YhG7Cw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     </head>
     <body class="d-flex flex-column min-vh-100">
-        @if(isset($exception->getHeaders()['navbar']))
-            @if($exception->getHeaders()['navbar'] == true)
-                @include('layout.navbar')
+        @if(isset($exception))
+            @if(isset($exception->getHeaders()['navbar']))
+                @if($exception->getHeaders()['navbar'] == true)
+                    @include('layout.navbar')
+                @endif
             @endif
         @else
             @include('layout.navbar')
@@ -34,9 +36,11 @@
         <div id="content">
             @yield('content')
         </div>
-        @if(isset($exception->getHeaders()['footer']))
-            @if($exception->getHeaders()['footer'] == true)
-                @include('layout.footer')
+        @if(isset($exception))
+            @if(isset($exception->getHeaders()['footer']))
+                @if($exception->getHeaders()['footer'] == true)
+                    @include('layout.footer')
+                @endif
             @endif
         @else
             @include('layout.footer')
